3975c3df8e70dd9083def8d04224c0a60f8a038d
Amilton Junior <amilton_junior@msn.com>
2018-07-19 14:58:59 -0300

    Estrutura inicial do projeto

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "OK";
+/**
+ * Sistema que faz a validação de um número de cartão de crédito
+ *
+ * Autor: Amilton Fontoura de Camargo Junior
+ * Data: Julho de 2018
+ */
+
+// Pula uma linha no começo do output
+echo "\n";
+
+// TODO
+
+// Pula duas linhas no fim do output
+echo "\n\n";
---
+++ run.command
@@ -0,0 +1,4 @@
+#! /bin/bash
+current_path="$(dirname "$BASH_SOURCE")"
+cd $current_path
+php index.php
