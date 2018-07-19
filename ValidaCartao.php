ce292dabf9b93292855f64c12880fa97826b156d
Amilton Junior <amilton_junior@msn.com>
2018-07-19 15:54:26 -0300

    Implementada a validação do número do cartão

--- ValidaCartao.php
+++ ValidaCartao.php
@@ -8,6 +8,84 @@
  */
 class ValidaCartao {
     
-    // TODO
+    // Atributos da classe
+    private $card_number = 0;
+    private $card_bank = '';
+    
+    // Método construtor
+    public function ValidaCartao() {}
+    
+    // Método que define o número do cartão
+    public function setCardNumber($number='') {
+        // Remove espaços, quebras de linha e tabulações do valor recebido
+        $number = $this->prepareCardNumber($number);
+
+        // Verifica se o atributo number é válido, de acordo com as regras
+        if (!$this->validateCardNumber($number))
+            return false;
+        
+        // Salva o número no atributo da classe
+        $this->card_number = $number;
+
+        // Identifica o banco emissor
+    }
+    
+    // Método que valida o número de um cartão de crédito
+    public function validateCardNumber($number='') {
+        // Remove espaços, quebras de linha e tabulações do valor recebido
+        $number = $this->prepareCardNumber($number);
+        
+        // Verifica se o número é válido, de acordo com as regras básicas
+        if (!is_numeric($number) || strlen($number) < 13 || strlen($number) > 16)
+            return false;
+        
+        // Verifica se a formação do número é, inicialmente, válido, de acordo com as regras dos bancos
+        if  (
+                !((strlen($number) == 15 && (substr($number, 0, 2) == '34' || substr($number, 0, 2) == '37')) // AMEX
+                || (strlen($number) == 16 && substr($number, 0, 4) == '6011') // Discover
+                || (strlen($number) == 16 && (substr($number, 0, 2) == '51' || substr($number, 0, 2) == '55')) // MasterCard
+                || (substr($number, 0, 1) == '4' && (strlen($number) == 13 || strlen($number) == 16))) // Visa
+            )
+            return false;
+        
+        // Faz a validação pelo algoritmo Luhn
+
+        // Inverte a sequência de números
+        $n_rev = strrev($number);
+        
+        // Inicializa as variáveis auxiliares
+        $aux    = array();
+        $total  = 0;
+        
+        // Dobra o valor de cada número em posição de índice ímpar e guarda no array
+        for ($i=0; $i<strlen($n_rev); $i++)
+            if ($i > 0 && $i % 2 != 0) {
+                // Dobra o valor
+                $value = (int) $n_rev[$i] * 2;
+                
+                // Se value for maior do que 9, subtrai o mesmo por 9
+                if ($value > 9)
+                    $aux[] = $value - 9;
+                else
+                    $aux[] = $value;
+            } else
+                $aux[] = (int) $n_rev[$i];
+        
+        // Faz a somatória de todos os números do array
+        foreach ($aux as $n)
+            $total += (int) $n;
+        
+        // Verifica se a soma é um número múltiplo de 10
+        if ($total %10 != 0)
+            return false;
+        
+        return true;
+    }
+    
+    // Método que limpa um número de cartão recebido
+    public function prepareCardNumber($number='') {
+        // Efetua as substituições
+        return str_replace(array(' ', '.', ',', '-', "\n", "\t"), '', trim($number));
+    }
     
 }
--- index.php
+++ index.php
@@ -7,10 +7,22 @@
  * Data: Julho de 2018
  */
 
+include_once('ValidaCartao.php');
+
 // Pula uma linha no começo do output
 echo "\n";
 
-// TODO
+$cartao = new ValidaCartao();
+
+$numero = '4111111111111111';
+
+echo 'Numero = '.$numero;
+echo "\nValido = ";
+
+if ($cartao->validateCardNumber($numero))
+    echo 'SIM';
+else
+    echo 'NAO';
 
 // Pula duas linhas no fim do output
 echo "\n\n";
